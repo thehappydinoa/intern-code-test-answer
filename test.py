8280e6ef28bf9df0ff7633db76f22c3a356b8991
Aidan Holland <thehappydinoa@gmail.com>
2018-04-20 12:11:53 -0400

    Unit test 3-6

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import json
 import unittest
 import requests
+from datetime import datetime, timezone
 from psycopg2 import connect, OperationalError, ProgrammingError, InternalError
 
 from settings import config
@@ -14,10 +15,18 @@ except OperationalError as e:
     exit(1)
 
 
-def psql_command(command, input=False, fetch=True, fetchall=False):
+def psql_command(command, values=False, fetch=True, fetchall=False):
+    if not command.endswith(';'):
+        command = command + ';'
     cur = conn.cursor()
     try:
-        cur.execute(command)
+        if values:
+            if not ';' in str(values):
+                cur.execute(command, values)
+            else:
+                raise InternalError("SQL Injection attempt")
+        else:
+            cur.execute(command)
         if fetch:
             if fetchall:
                 response = cur.fetchall()
@@ -26,6 +35,7 @@ def psql_command(command, input=False, fetch=True, fetchall=False):
         else:
             response = None
     except (ProgrammingError, InternalError) as e:
+        print(str(e))
         response = None
     conn.commit()
     cur.close()
@@ -55,7 +65,7 @@ def add_item():
     title = item["item"]["title"]
     categoryId = item["item"]["categoryId"]
     response = psql_command(
-        "INSERT INTO items(title, category_id) VALUES(%s, %s) returning *;", input=(title, categoryId))
+        "INSERT INTO items(title, category_id) VALUES(%s, %s) returning *;", values=(title, categoryId))
     return serializeItem(response)
 
 
@@ -68,13 +78,25 @@ def get_items():
     return result
 
 
+def get_item(item_id):
+    response = psql_command(
+        "SELECT * FROM items WHERE ID = %s;", values=(item_id,))
+    try:
+        result = serializeItem(response)
+        return json.dumps(result)
+    except IndexError:
+        result = None
+        return result
+
+
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         self.base_url = "http://localhost:" + str(config["port"])
+        psql_command('TRUNCATE items RESTART IDENTITY', fetch=False)
 
     def test_get_index(self):
-        psql_command('TRUNCATE items RESTART IDENTITY', fetch=False)
         r = requests.get(self.base_url + '/')
+        assert str(r.status_code) in "200"
         assert 'OK' in r.json()['status']
 
     def test_get_items(self):
@@ -82,13 +104,46 @@ class FlaskTestCase(unittest.TestCase):
         item2 = add_item()
         expected = json.dumps(get_items())
         r = requests.get(self.base_url + '/items')
+        assert str(r.status_code) in "200"
         assert expected in r.text
 
-    def test_get_make(self):
+    def test_get_item(self):
         item3 = add_item()
         expected = json.dumps(item3)
-        r = requests.get(self.base_url + '/items/' + item3["item"]["id"])
+        r = requests.get(self.base_url + '/items/' + str(item3["item"]["id"]))
+        assert str(r.status_code) in "200"
         assert expected in r.text
+
+    def test_post_items(self):
+        item = {
+            "item": {
+                "title": 'Some new title here',
+                "categoryId": 2
+            }
+        }
+        r = requests.post(self.base_url + '/items', data=json.dumps(item))
+        expected = get_item(r.json()["item"]["id"])
+        assert str(r.status_code) in "200"
+        assert expected in r.text
+
+    def test_put_item(self):
+        item = {"item": {"title": 'new title'}}
+        item4 = add_item()
+        expected = item["item"]["title"]
+        r = requests.put(self.base_url + '/items/' +
+                         str(item4["item"]["id"]), data=json.dumps(item))
+        assert str(r.status_code) in "200"
+        assert expected in r.json()["item"]["title"]
+
+    def test_delete_item(self):
+        item5 = add_item()
+        r = requests.delete(self.base_url + '/items/' +
+                            str(item5["item"]["id"]))
+        response = psql_command(
+            'select exists(select 1 from items where id=%s)', values=(item5["item"]["id"],))
+        assert str(r.status_code) in "200"
+        assert "{}" in r.text
+        assert None not in response
 
 
 if __name__ == '__main__':
